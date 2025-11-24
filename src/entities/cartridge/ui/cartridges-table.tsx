1663009ed0068d22205d3d89ec4e55962ee4b2b3
smetanin-e <e91smet15@gmail.com>
2025-11-24 12:06:00 +0300

    fix

--- src/entities/cartridge/ui/cartridges-table.tsx
+++ src/entities/cartridge/ui/cartridges-table.tsx
@@ -39,39 +39,6 @@ export const CartridgesTable: React.FC<Props> = ({ count, items }) => {
           ))}
         </>
       )}
-      {/* <Table>
-        <TableHeader>
-          <TableRow>
-            <TableHead>Номер</TableHead>
-            <TableHead>Модель</TableHead>
-            <TableHead>Статус</TableHead>
-            <TableHead className='text-right'>Действия</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {count === 0 ? (
-            <TableRow>
-              <TableCell colSpan={4} className='text-center py-8 text-muted-foreground'>
-                Картриджи не найдены
-              </TableCell>
-            </TableRow>
-          ) : (
-            <>
-              {items.map((cartridge) => (
-                <TableRow key={cartridge.id}>
-                  <TableCell className='font-medium'>{cartridge.label}</TableCell>
-                  <TableCell>{cartridge.model?.model}</TableCell>
-                  <TableCell>{getStatusBadge(cartridge.status)}</TableCell>
-                  <TableCell className='flex items-center justify-end gap-6'>
-                    <ChangeCartridgeStatus id={cartridge.id} currentStatus={cartridge.status} />
-                    <DeleteCartridge cartridgeId={cartridge.id} cartridgeLabel={cartridge.label} />
-                  </TableCell>
-                </TableRow>
-              ))}
-            </>
-          )}
-        </TableBody>
-      </Table> */}
     </div>
   );
 };
