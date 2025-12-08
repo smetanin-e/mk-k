9cba92ce75e48494fea360878efe7ca28decb9e1
smetanin-e <e91smet15@gmail.com>
2025-12-08 14:52:39 +0300

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'standalone',
+  experimental: {
+    serverActions: {
+      allowedOrigins: ['cartridges.kok.mak', '172.17.4.43', 'localhost:3000'],
+    },
+  },
 };
 
 export default nextConfig;
